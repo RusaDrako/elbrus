3c2824d5994f894458f46985bfaed312d2332d34
RusaDrako <f182@rambler.ru>
2020-04-16 16:41:18 +0400

    Добавлены базовые шаблоны

---
+++ template/template_native/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $this->title; ?></title>
+
+<link rel="shortcut icon" href="/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="/css/_skin/_elbrus_start/reset.css" />
+<link rel="stylesheet" type="text/css" href="/css/_skin/_elbrus_start/css.css" />
+
+</head>
+
+
+
+
+
+<body>
+
+	<div class="body">
+		<div class="content">
+			<h1><?php echo $this->title; ?></h1>
+
+			<?php $this->include('index_menu.php', []); ?>
+		&nbsp;
+		</div>
+	&nbsp;
+	</div>
+
+</body>
+</html>
---
+++ template/template_native/index_menu.php
@@ -0,0 +1,4 @@
+<a href="/">Главная</a>
+<!--a href="/___test/">Тестовая страница</a-->
+<!--a href="/test/news/123">Блог</a-->
+<a href="https://github.com/RusaDrako/elbrus" target="_blank">GitHub</a>
