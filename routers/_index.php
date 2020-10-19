08fc0f0e28e2e9b8c15bd974a5c2e0b1b872419f
Petukhov Leonid <test@test.ru>
2020-10-19 12:45:27 +0400

    Перенастройка маршрутизатора.

--- routers/_index.php
+++ routers/_index.php
@@ -2,30 +2,46 @@
 
 use \resources\router\router			as router;
 
-
 \view::call(resources\template_native\view_template_native::call());
 
 
 
 
 
+# Формируем базовые переменные для шаблона
+$header = [
+	'title' => \registry::call()->get('_ELBRUS_TITLE') . ' (ver. ' . \registry::call()->get('_ELBRUS_VERSION') . ')',
+];
+
+\view::call()->variable('header', $header);
+
+
+
+
+
+$func_def = function () {
+	\view::call()
+			->display('index');
+};
+# Заглавная страница (в случае необходимости можно переопределить далее)
+router::call()->default_page($func_def);
+
+
+
+
+
+# Получаем имя группы
+$group = router::call()->get_group();
 # Файл группы маршрутов
-$file_name = \registry::call()->get('FOLDER_ROOT') . 'routers/' . router::call()->get_group() . '.php';
+$file_name = \registry::call()->get('FOLDER_ROOT') . 'routers/' . $group . '.php';
 # Если файл группы маршрутов существует
 if (file_exists($file_name)) {
 	# Подгружаем маршруты
 	require_once($file_name);
 } else {
-	$func = function () {
-		$content = \registry::call()->get('_ELBRUS_TITLE') . ' (v' . \registry::call()->get('_ELBRUS_VERSION') . ')';
-
-		\view::call()->variable('title', $content)
-				->display('index');
-	};
 	# Заглавная страница
-	router::call()->any('/', $func);
+	router::call()->any('/', $func_def);
 }
-
 
 
 
@@ -33,7 +49,3 @@ if (file_exists($file_name)) {
 
 # Вызов обработчика маршрутизатора (перед этим указываем страницу по умолчанию)
 $content = router::call()->router();
-
-
-
-?>
