8ad88c4053a0c8a8302f9441f5ea1ad486299d04
Petukhov Leonid <rusadrako@yandex.ru>
2021-03-14 18:34:17 +0400

    Удаление старых файлов

--- app/_template/template_native/index_menu.php
+++
@@ -1,4 +0,0 @@
-<a href="/">Главная</a>
-<!--a href="/___test/">Тестовая страница</a-->
-<!--a href="/test/news/123">Блог</a-->
-<a href="https://github.com/RusaDrako/elbrus" target="_blank">GitHub</a>
