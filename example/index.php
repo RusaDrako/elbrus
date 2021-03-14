9ae5d603b920d3629b3b77d34a23ae8198138a08
Petukhov Leonid <rusadrako@yandex.ru>
2021-03-14 18:33:43 +0400

    Примеры

---
+++ example/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . '/../src/autoload.php');
+
+new test();
+
+//print_info('Ядро загружено', 'Ядро загружено', 1);
+
+//print_info(\registry::call(), 'registry');
+
+//print_info(\request::call(), 'request');
+
+//print_info($_SERVER, '$_SERVER');
